4460eca7e2f36924a8530c099bb60c59c2880d89
Muya Chang <mchang87@gatech.edu>
2021-08-27 21:25:35 -0400

    Added this file

---
+++ Lib/ASCII.h
@@ -0,0 +1,39 @@
+
+#ifndef _ASCII_H_
+#define _ASCII_H_
+
+#define   NUL 0x00 // Ctrl-@
+#define   SOH 0x01 // Ctrl-A
+#define   STX 0x02 // Ctrl-B
+#define   ETX 0x03 // Ctrl-C
+#define   EOT 0x04 // Ctrl-D
+#define   ENQ 0x05 // Ctrl-E
+#define   ACK 0x06 // Ctrl-F
+#define   BEL 0x07 // Ctrl-G
+#define    BS 0x08 // Ctrl-H
+#define    HT 0x09 // Ctrl-I
+#define    LF 0x0A // Ctrl-J
+#define    VT 0x0B // Ctrl-K
+#define    FF 0x0C // Ctrl-L
+#define    CR 0x0D // Ctrl-M
+#define    SO 0x0E // Ctrl-N
+#define    SI 0x0F // Ctrl-O
+#define   DLE 0x10 // Ctrl-P
+#define   DC1 0x11 // Ctrl-Q
+#define   DC2 0x12 // Ctrl-R
+#define   DC3 0x13 // Ctrl-S
+#define   DC4 0x14 // Ctrl-T
+#define   NAK 0x15 // Ctrl-U
+#define   SYN 0x16 // Ctrl-V
+#define   ETB 0x17 // Ctrl-W
+#define   CAN 0x18 // Ctrl-X
+#define    EM 0x19 // Ctrl-Y
+#define   SUB 0x1A // Ctrl-Z
+#define   ESC 0x1B // Ctrl-[
+#define    FS 0x1C // Ctrl-'\'
+#define    GS 0x1D // Ctrl-]
+#define    RS 0x1E // Ctrl-^
+#define    US 0x1F // Ctrl-_
+#define SPACE 0x20 // Whitespace
+
+#endif
