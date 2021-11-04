c84c0fbfb596988480de3d23fce8ac93818a156c
Muya Chang <mchang87@gatech.edu>
2021-11-03 20:55:27 -0400

    Improve readibility

--- Descriptors.c
+++ Descriptors.c
@@ -36,7 +36,6 @@ const USB_Descriptor_Device_t PROGMEM DeviceDescriptor =
   .Endpoint0Size          = FIXED_CONTROL_ENDPOINT_SIZE,
     
   .VendorID               = 0x03EB, // Atmel
-
   .ProductID              = 0x204B, // LUFA USB to Serial Demo Application
   .ReleaseNumber          = 0x0001,
     
